40a6957d44eb775d98f2e991ec44bcaa1a428056
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2026-01-28 18:42:19 +0600

    Added z-index to floating header

--- src/components/layouts/mainLayout/MainLayotHeader.tsx
+++ src/components/layouts/mainLayout/MainLayotHeader.tsx
@@ -2,7 +2,7 @@ import { CardNav } from "@/components/ui";
 
 export const MainLayoutHeader = () => {
   return (
-    <div className="relative py-12">
+    <div className="sticky top-0 py-12 z-9999">
       <CardNav ease="power3.out" />
     </div>
   );
